5a4a0c5e9ea1370ba6f85fc709bd03eca30e0756
Rick305 <65296647+Rick305@users.noreply.github.com>
2023-05-03 20:58:23 +0200

    last changes

--- src/i18n.js
+++ src/i18n.js
@@ -19,7 +19,7 @@ i18n
         translation: {
           from: "from",
           to: "to",
-          convince: "convince me",
+          convince: "Let's Go",
           title: "Your ride. Your decision.",
           text: "Taking the bike is good for the environment, your health and your wallet! For this <1>{{distance}}</1> bike ride you'll burn <3>{{calories}}</3> calories, help nature with <5>{{co2}}</5> less CO2 polution and on top of that save <7>€ {{money}}</7>!",
           new: "new route",
@@ -31,7 +31,7 @@ i18n
         translation: {
           from: "von",
           to: "nach",
-          convince: "überzeuge mich",
+          convince: "Auf geht's",
           title: "Deiner Weg. Deine Entscheidung.",
           text: "Das Fahrrad ist nicht nur gut für die Umwelt und deine Gesundheit, sondern auch für deinen Geldbeutel. Mit der geplanten Fahrradstrecke von <1>{{distance}}</1> verbrennst du <3>{{calories}}</3> Kalorien, entlastest die Umwelt um <5>{{co2}}</5> CO2 und sparst zudem <7>€ {{money}}</7> an Sprit!",
           new: "neue Berechnung",
